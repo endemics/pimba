b3adb9b0703c9c31a0d4fba5a8a54c501e47cc45
Gildas Le Nadan <gildas@endemic-systems.com>
2020-02-04 20:18:31 +1000

    Removing qemu since we are planning to use travis arm64 platform Also removing packer arm plugin for the same reason Using latest packer

--- bin/travis-packer.sh
+++ bin/travis-packer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=${PACKER_VERSION:=1.4.2}
+VERSION=${PACKER_VERSION:=1.5.1}
 
 if echo "${VERSION}" | cmp --silent ${HOME}/packer/.build -; then
   echo "packer ${VERSION} up to date!"
@@ -15,14 +15,7 @@ rm -rf packer
 mkdir -p packer/{bin,build}
 
 # Checking for a tarball before downloading makes testing easier :-)
-test -f "packer_${VERSION}_linux_amd64.zip" || wget "https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip"
-unzip "packer_${VERSION}_linux_amd64.zip" -d packer/bin
-
-cd ${HOME}/packer/build
-git clone https://github.com/solo-io/packer-builder-arm-image
-cd packer-builder-arm-image
-go mod download
-go build
-mv packer-builder-arm-image ${HOME}/packer/bin/packer-builder-arm-image
+test -f "packer_${VERSION}_linux_arm64.zip" || wget "https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_arm64.zip"
+unzip "packer_${VERSION}_linux_arm64.zip" -d packer/bin
 
 echo "${VERSION}" > ${HOME}/packer/.build
