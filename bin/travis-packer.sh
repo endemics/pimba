88f6216e91a3a75e1eb0c44b14f0804255b7c47c
Gildas Le Nadan <gildas@endemic-systems.com>
2020-02-05 20:23:27 +1000

    restore packer-builder-arm-image since we need it

--- bin/travis-packer.sh
+++ bin/travis-packer.sh
@@ -2,20 +2,29 @@
 set -e
 
 VERSION=${PACKER_VERSION:=1.5.1}
+ARCH=${PACKER_ARCH:=arm64}
 
-if echo "${VERSION}" | cmp --silent ${HOME}/packer/.build -; then
-  echo "packer ${VERSION} up to date!"
+if echo "${VERSION}-${ARCH}" | cmp --silent ${HOME}/packer/.build -; then
+  echo "packer ${VERSION} for ${ARCH} is up to date!"
   exit 0
 fi
 
 echo "VERSION: ${VERSION}"
+echo "ARCH: ${ARCH}"
 
 cd ${HOME}
 rm -rf packer
 mkdir -p packer/{bin,build}
 
 # Checking for a tarball before downloading makes testing easier :-)
-test -f "packer_${VERSION}_linux_arm64.zip" || wget "https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_arm64.zip"
-unzip "packer_${VERSION}_linux_arm64.zip" -d packer/bin
+test -f "packer_${VERSION}_linux_${ARCH}.zip" || wget "https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_${ARCH}.zip"
+unzip "packer_${VERSION}_linux_${ARCH}.zip" -d packer/bin
 
-echo "${VERSION}" > ${HOME}/packer/.build
+cd ${HOME}/packer/build
+git clone https://github.com/solo-io/packer-builder-arm-image
+cd packer-builder-arm-image
+go mod download
+go build
+mv packer-builder-arm-image ${HOME}/packer/bin/packer-builder-arm-image
+
+echo "${VERSION}-${ARCH}" > ${HOME}/packer/.build
