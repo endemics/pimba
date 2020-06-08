c61c21ea4d98f1dd5b325b4a5ff0f201457d7531
Gildas Le Nadan <gildas@endemic-systems.com>
2020-06-08 21:18:52 +1000

    Make linter happier

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ lint:
 	  $(PWD):/src \
 	  -w /src \
 	  koalaman/shellcheck:v0.7.0 \
-	    -e SC2154 \
 	    files/pimusicbox/startup.sh \
-	    files/pimusicbox/bin/network.sh
+	    files/pimusicbox/bin/network.sh \
+	    bin/shrink-img.sh
 
 img:
 	sudo env "PATH=$(PATH):$(HOME)/packer/bin:$(HOME)/qemu/bin:/usr/sbin:/sbin" ~/packer/bin/packer build packer.json
--- bin/shrink-img.sh
+++ bin/shrink-img.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function check_bin() {
-    if ! which $1 > /dev/null ; then
+    if ! hash "$1" > /dev/null ; then
         echo "ERROR: $1 must be installed"
         return 1
     fi
@@ -25,37 +25,52 @@ function shrink() {
     check_bin parted
     check_bin truncate
 
-    local OLD_SIZE=$(ls -lh $IMG_FILE | cut -d' ' -f5)
+    local OLD_SIZE
+    local BLOCK_SIZE
+    local MIN_BLOCKS
+    local EXTRA_BLOCKS
+    local SIZE_BLOCKS
+    local LOOP_DEV
+    local SIZE_BYTES
+    local FIRST_BYTE
+    local LAST_BYTE
+    local FINAL_SIZE
+    local NEW_SIZE
+
+    # shellcheck disable=SC2012
+    OLD_SIZE=$(ls -lh "$IMG_FILE" | cut -d' ' -f5)
 
     echo "INFO: Shrinking $IMG_FILE..."
-    kpartx -sa $IMG_FILE
+    kpartx -sa "$IMG_FILE"
 
-    local LOOP_DEV=$(losetup -O NAME,BACK-FILE -l -n | grep $IMG_FILE | sed "s#^/dev/\(loop[0-9]*\) *.*#\1#")
+
+    LOOP_DEV=$(losetup -O NAME,BACK-FILE -l -n | grep "$IMG_FILE" | sed "s#^/dev/\(loop[0-9]*\) *.*#\1#")
     local PART_NUM=2
     local ROOT_PART=/dev/mapper/${LOOP_DEV}p${PART_NUM}
-    e2fsck -fy $ROOT_PART
+    e2fsck -fy "$ROOT_PART"
 
-    local BLOCK_SIZE=$(tune2fs -l $ROOT_PART | awk '/^Block size/ {print $NF}')
-    local MIN_BLOCKS=$(resize2fs -P $ROOT_PART | awk -F': ' '{print $2}')
+    BLOCK_SIZE=$(tune2fs -l "$ROOT_PART" | awk '/^Block size/ {print $NF}')
+    MIN_BLOCKS=$(resize2fs -P "$ROOT_PART" | awk -F': ' '{print $2}')
 
     # 20MB of extra free space
-    local EXTRA_BLOCKS=$(echo "1024 * 1024 * 20 / $BLOCK_SIZE" | bc)
-    local SIZE_BLOCKS=$(echo "$MIN_BLOCKS + $EXTRA_BLOCKS" | bc)
-    resize2fs $ROOT_PART $SIZE_BLOCKS
+    EXTRA_BLOCKS=$(echo "1024 * 1024 * 20 / $BLOCK_SIZE" | bc)
+    SIZE_BLOCKS=$(echo "$MIN_BLOCKS + $EXTRA_BLOCKS" | bc)
+    resize2fs "$ROOT_PART" "$SIZE_BLOCKS"
     sync && sleep 1
-    kpartx -d $IMG_FILE
+    kpartx -d "$IMG_FILE"
 
-    local SIZE_BYTES=$(echo "$SIZE_BLOCKS * $BLOCK_SIZE" | bc)
-    local FIRST_BYTE=$(parted -m $IMG_FILE unit B print | tail -1 | cut -d':' -f2 | tr -d 'B')
-    local LAST_BYTE=$(echo "$FIRST_BYTE + $SIZE_BYTES" | bc)
+    SIZE_BYTES=$(echo "$SIZE_BLOCKS * $BLOCK_SIZE" | bc)
+    FIRST_BYTE=$(parted -m "$IMG_FILE" unit B print | tail -1 | cut -d':' -f2 | tr -d 'B')
+    LAST_BYTE=$(echo "$FIRST_BYTE + $SIZE_BYTES" | bc)
 
-    parted $IMG_FILE rm $PART_NUM
-    parted $IMG_FILE unit B mkpart primary $FIRST_BYTE $LAST_BYTE
-    local FINAL_SIZE=$(parted -m $IMG_FILE unit B print free | tail -1 | cut -d':' -f2 | tr -d 'B')
-    truncate --size $FINAL_SIZE $IMG_FILE
+    parted "$IMG_FILE" rm "$PART_NUM"
+    parted "$IMG_FILE" unit B mkpart primary "$FIRST_BYTE" "$LAST_BYTE"
+    FINAL_SIZE=$(parted -m "$IMG_FILE" unit B print free | tail -1 | cut -d':' -f2 | tr -d 'B')
+    truncate --size "$FINAL_SIZE" "$IMG_FILE"
     sync && sleep 1
 
-    local NEW_SIZE=$(ls -lh $IMG_FILE | cut -d' ' -f5)
+    # shellcheck disable=SC2012
+    NEW_SIZE=$(ls -lh "$IMG_FILE" | cut -d' ' -f5)
     echo "INFO: Reduced $IMG_FILE from $OLD_SIZE to $NEW_SIZE"
     return 0
 }
